5107166e8908c08538a9c5ba1d7312ad73262e68
phlpphcknr <philipp.hackner@protonmail.com>
2021-06-24 22:24:12 +0200

    Clean up

---
+++ src/components/Sphere.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/macro";
+
+const Sphere = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 80px;
+  margin: 10px;
+  background-color: var(--secondary-color);
+  border-radius: 50%;
+`;
+
+export default Sphere;
